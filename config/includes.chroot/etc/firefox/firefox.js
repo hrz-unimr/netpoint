1e2c26b8552599c1ef1ec1b72d44a22f6647ece4
Stefan Schneider <eqsoftdev@gmail.com>
2016-03-12 13:42:47 +0100

    some important security fixes

--- config/includes.chroot/etc/firefox/firefox.js
+++ config/includes.chroot/etc/firefox/firefox.js
@@ -1,15 +1,3 @@
-// This is the Debian specific preferences file for Iceweasel
-// You can make any change in here, it is the purpose of this file.
-// You can, with this file and all files present in the
-// /etc/iceweasel/pref directory, override any preference that is
-// present in /usr/lib/iceweasel/defaults/preferences directory.
-// While your changes will be kept on upgrade if you modify files in
-// /etc/iceweasel/pref, please note that they won't be kept if you
-// do make your changes in /usr/lib/iceweasel/defaults/preferences.
-//
-// Note that lockPref is allowed in these preferences files if you
-// don't want users to be able to override some preferences.
-
 lockPref("extensions.update.enabled", false);
 
 // Use LANG environment variable to choose locale
@@ -26,7 +14,7 @@ lockPref("browser.fullscreen.autohide",false);
 // Disable Health Report
 lockPref("datareporting.healthreport.service.enabled",false);
 // Disable platform filepicker 
-// (see also disabled chrome://global/content/filepicker.xul in /usr/lib/iceweasel/browser/chrome.manifest)
+// (see also disabled chrome://global/content/filepicker.xul in /usr/lib/firefox/browser/chrome.manifest)
 lockPref("ui.allow_platform_file_picker",false);
 // from porteus
 lockPref("accessibility.browsewithcaret_shortcut.enabled", false);
